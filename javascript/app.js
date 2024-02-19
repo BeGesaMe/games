35588ba1d179f0dfbefbc32371a3356277131cb4
BeGesaMe <bekjonshavkatov0@gmail.com>
2024-02-19 15:46:03 +0500

    dafds

--- javascript/app.js
+++ javascript/app.js
@@ -1,44 +1,78 @@
 let API_KEY = '5ab96142ac1f43cc9fd0f196aa03d7a7';
 
-let API_URL = `https://api.rawg.io/api/platforms?key=${API_KEY}`
+let GENRES_API = `https://api.rawg.io/api/genres?key=`
+let PLATFORMS_URL = `https://api.rawg.io/api/platforms?key=`
+let GAMES_API = `https://api.rawg.io/api/games?key=`
+let CREATORS = `https://api.rawg.io/api/creators/`
 
-fetch(API_URL)
+
+fetch(GENRES_API + API_KEY)
+    .then(res => res.json())
+        .then(data =>{
+            data.results.forEach( (el)=>{
+                let nodeCLoneTemplate = templateRek.cloneNode(true);
+                let genresText = nodeCLoneTemplate.querySelector('h2')
+                let genresImg = nodeCLoneTemplate.querySelector('#rasm')
+                genresText.innerHTML = el.name
+                genresImg.src = el.image_background
+                genres1.appendChild(nodeCLoneTemplate)
+            })
+})
+
+fetch(PLATFORMS_URL + API_KEY)
     .then(res => res.json())
         .then(data =>{
 
-            console.log(data);
-           data.results.forEach( (el) => {
+            data.results.forEach( (el)=>{
 
-                let cloneTemplate = templateRek.cloneNode(true)
+                let nodeCLoneTemplate2 = templateGenres2.cloneNode(true)
 
-                cloneTemplate.querySelector('img').src = el.image_background;
+                let info = nodeCLoneTemplate2.querySelector('option')
 
-                 let gameTitle =  cloneTemplate.querySelector('h2');
+                info.innerHTML = el.name 
 
-                 gameTitle.innerHTML = el.name 
+                GenresBox.appendChild(nodeCLoneTemplate2)
 
-                 genres.appendChild(cloneTemplate)
+            })
+})
 
-           });
+let salom = fetch(GAMES_API + API_KEY)
+    .then(res => res.json())
+        .then(data =>{
+            console.log(data.results);
+
+            data.results.forEach( (el)=>{
+                let nodeCLoneTemplateCards = templateCard.cloneNode(true)
+
+                let cardImg = nodeCLoneTemplateCards.querySelector('img')
+                let cardTitle = nodeCLoneTemplateCards.querySelector('.game-card-title')
+                let cardCount = nodeCLoneTemplateCards.querySelector('.game-card-games-count')
+
+                cardTitle.innerHTML = el.name 
+                cardImg.src = el.background_image
+                cardCount.innerHTML = el.metacritic
+
+                    cardTitle.addEventListener('click',()=>{
+                        
+                        // location.href = "part.html"
+
+                        console.log(el.slug);
+
+                        salom = GAMES_API + API_KEY + `//${el.slug}`
+
+                        console.log(salom);
+
+                        fetch('https://api.rawg.io/api/games?key=5ab96142ac1f43cc9fd0f196aa03d7a7//the-witcher-3-wild-hunt')
+                            .then(res => res.json())
+                                .then(data =>{
+                                    console.log(data);
+                                })
+                        
+                    })
+
+
+                gamesCards.appendChild(nodeCLoneTemplateCards)
+            })
 })
 
 
-fetch(API_URL)
-    .then(r => r.json())
-        .then(result =>{
-
-            result.results.forEach((el)=>{
-            
-
-                let cloneTemplate = templateCard.cloneNode(true)
-
-                let gamesImg = cloneTemplate.querySelector('img')
-
-                gamesImg.src = el.image_background
-
-                let gamesTitle = cloneTemplate.querySelector('.game-card-title')
-                gamesTitle.innerHTML = el.name
-
-                gamesCards.appendChild(cloneTemplate)
-
-})})
--- javascript/uzgaruvchilar.js
+++ javascript/uzgaruvchilar.js
@@ -2,7 +2,14 @@ let gamesCards = document.querySelector('.games-cards');
 let templateCard = document.querySelector('.template-card').content;
 let newFragment = new DocumentFragment();
 
-
 let templateRek = document.querySelector('.template-rek').content;
-let genres = document.querySelector('.genres');
+let genres1 = document.querySelector('.genres');
 let newFragment2 = new DocumentFragment();
+
+let templateGenres2 = document.querySelector('.template-genres2').content
+let GenresBox = document.querySelector('.genres2')
+let newFragment3 = new DocumentFragment()
+
+
+
+
